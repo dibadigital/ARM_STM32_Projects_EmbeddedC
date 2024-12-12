76c4da63cb5451ee7e97e6e34e4c1810fb7d5808
dibadigital <107550965+dibadigital@users.noreply.github.com>
2024-12-12 15:59:34 +0330

    Added all

---
+++ DAC_PlayMusic_16000_KEIL/LIB/Erfan.h
@@ -0,0 +1,20 @@
+#ifndef Erfan_H_
+#define Erfan_H_
+
+/*------------------------------------
+---Mehran Msba Audio File Generator---
+------------------------------------*/
+#include <stdint.h>
+
+
+#define   Erfan_LEN   499780U //(49981 - 4 + 1) * 10 = 499780
+
+
+
+
+extern const uint16_t Erfan[];
+
+
+
+#endif
+
---
+++ DAC_PlayMusic_16000_KEIL/LIB/beep.h
@@ -0,0 +1,17 @@
+#ifndef beep_H_
+#define beep_H_
+
+#include <stdint.h>
+
+
+#define   beep_LEN   3135U
+
+
+
+
+extern const uint16_t beep[];
+
+
+
+#endif
+
