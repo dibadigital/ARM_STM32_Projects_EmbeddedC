111a506dcbaec0c3df3c9d3a936528528d60eff0
dibadigital <107550965+dibadigital@users.noreply.github.com>
2024-12-12 15:08:54 +0330

    Added all

---
+++ WR_MemI2C_ExtEEPROM_KEIL/MDK-ARM/RTE/_WR_MemI2C_ExtEEPROM/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'WR_MemI2C_ExtEEPROM' 
+ * Target:  'WR_MemI2C_ExtEEPROM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
