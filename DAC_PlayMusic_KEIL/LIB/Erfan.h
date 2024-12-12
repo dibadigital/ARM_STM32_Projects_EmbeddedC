bcb436f1e11694a9cf28399f8c7686cc2825fb35
dibadigital <107550965+dibadigital@users.noreply.github.com>
2024-12-12 15:53:37 +0330

    Added all

---
+++ DAC_PlayMusic_KEIL/LIB/Erfan.h
@@ -0,0 +1,20 @@
+#ifndef Erfan_H_
+#define Erfan_H_
+
+/*------------------------------------
+---Mehran Msba Audio File Generator---
+------------------------------------*/
+#include <stdint.h>
+
+
+#define   Erfan_LEN   449780U  // size of array = (44981-4+1)*10 = 449780
+
+
+
+
+extern const uint16_t Erfan[];
+
+
+
+#endif
+
