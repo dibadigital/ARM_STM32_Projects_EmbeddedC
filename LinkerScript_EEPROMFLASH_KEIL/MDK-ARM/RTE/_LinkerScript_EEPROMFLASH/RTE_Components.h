0b4f74bfd846b2582bbcf929a5cdc481ce99986a
dibadigital <107550965+dibadigital@users.noreply.github.com>
2024-12-12 14:17:03 +0330

    Added all

---
+++ LinkerScript_EEPROMFLASH_KEIL/MDK-ARM/RTE/_LinkerScript_EEPROMFLASH/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'LinkerScript_EEPROMFLASH' 
+ * Target:  'LinkerScript_EEPROMFLASH' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
